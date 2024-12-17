d5c4c9c6df06fd2d2826bfb8714c52430ce8a416
Victor Baggio <victor.baggio@totvs.com.br>
2024-12-17 11:07:34 -0300

    feat: Iniciando o projeto

---
+++ src/main/java/com/baggiovictor/creditsimulation/CreditSimulationApplication.java
@@ -0,0 +1,13 @@
+package com.baggiovictor.creditsimulation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditSimulationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CreditSimulationApplication.class, args);
+  }
+
+}
